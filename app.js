d77f6036c1792c4485fe1fe20ed186120e3d5fe2
ICE <macintosh@macs-MacBook-Pro.local>
2022-06-10 10:36:34 +0100

    my first comit

---
+++ app.js
@@ -0,0 +1,28 @@
+let bars = document.querySelector(".fa-bars");
+let navSec = document.querySelector(".nav_section");
+
+bars.addEventListener("click", function () {
+  navSec.classList.toggle("show");
+});
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  autoplay: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
